f23d669f89e2822d4c59589ab0bb2dc9361a44de
Jenkin Joyer Dsouza <jalann@gmail.com>
2014-04-22 11:39:13 +0800

    added lamp installer

---
+++ lamp-installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get install lamp-server^
+
+echo -n "Add your user to the www-data group"
+sudo usermod -a -G www-data $USER
+
+echo -n "Add the /var/www folder to the www-data group"
+sudo chgrp -R www-data /var/www
+
+echo -n "Give write permissions to the www-data group"
+sudo chmod -R g+w /var/www
