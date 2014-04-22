7e739a41a1cdaab31380047d473562d35f2aa687
Jenkin Joyer Dsouza <jalann@gmail.com>
2014-04-22 11:54:51 +0800

    Fixed new line and default.conf

--- virtual-host-creator.sh
+++ virtual-host-creator.sh
@@ -13,21 +13,21 @@ localsiteport=$2
 deploy_dir=/var/www/$domain_name/public_html
 
 if [ ${#localsite} -eq 0 ]; then
-	echo -n "Creating www dir"
+	echo -n "Creating www dir \n"
 	sudo mkdir -p $deploy_dir
 
-	echo -n "Setting permissions"
+	echo -n "\n Setting permissions \n"
 	sudo chown -R $USER:$USER $deploy_dir
 
 	sudo chmod -R 755 /var/www
 
-	echo -n "Copying index.html file"
+	echo -n "\n Copying index.html file \n"
 	sudo cp vhc-includes/index.html $deploy_dir
-	echo -n "Create the New Virtual Host File"
-	sudo cp /etc/apache2/sites-available/default /etc/apache2/sites-available/$domain_name
-	echo -n "Please edit the new virtual file at /etc/apache2/sites-available/$domain_name"
+	echo -n "\n Create the New Virtual Host File \n"
+	sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$domain_name
+	echo -n "\n Please edit the new virtual file at /etc/apache2/sites-available/$domain_name"
 else
-	echo -n "Create the New Virtual Host File"
+	echo -n "\n Create the New Virtual Host File \n"
 	sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$domain_name-$localsiteport
 	echo -n "Please edit the new virtual file at /etc/apache2/sites-available/$domain_name-$localsiteport"
 fi
