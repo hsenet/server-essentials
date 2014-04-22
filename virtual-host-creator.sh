6db6a6e768206bea948b3b277ef2adda53b7968d
Jenkin Joyer Dsouza <jalann@gmail.com>
2014-04-22 11:57:53 +0800

    New lines removed

--- virtual-host-creator.sh
+++ virtual-host-creator.sh
@@ -16,20 +16,20 @@ if [ ${#localsite} -eq 0 ]; then
 	echo -n "Creating www dir \n"
 	sudo mkdir -p $deploy_dir
 
-	echo -n "\n Setting permissions \n"
+	echo -n "Setting permissions \n"
 	sudo chown -R $USER:$USER $deploy_dir
 
 	sudo chmod -R 755 /var/www
 
-	echo -n "\n Copying index.html file \n"
+	echo -n "Copying index.html file \n"
 	sudo cp vhc-includes/index.html $deploy_dir
-	echo -n "\n Create the New Virtual Host File \n"
+	echo -n "Create the New Virtual Host File \n"
 	sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$domain_name
-	echo -n "\n Please edit the new virtual file at /etc/apache2/sites-available/$domain_name"
+	echo -n "Please edit the new virtual file at /etc/apache2/sites-available/$domain_name \n"
 else
-	echo -n "\n Create the New Virtual Host File \n"
+	echo -n "Create the New Virtual Host File \n"
 	sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$domain_name-$localsiteport
-	echo -n "Please edit the new virtual file at /etc/apache2/sites-available/$domain_name-$localsiteport"
+	echo -n "Please edit the new virtual file at /etc/apache2/sites-available/$domain_name-$localsiteport \n"
 fi
 
 
