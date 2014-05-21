fa74c6e70d66ff8080121f9ca51328b127c1758e
Jenkin Joyer Dsouza <jalann@gmail.com>
2014-05-21 15:14:45 +0800

    Cleaned up generation of vertualhost file

--- virtual-host-creator.sh
+++ virtual-host-creator.sh
@@ -13,26 +13,29 @@ localsiteport=$2
 deploy_dir=/var/www/$domain_name/public_html
 log_dir=/var/www/$domain_name/logs
 
+cp /dev/null vhc-includes/virtual-host.conf
+
 if [ ${#localsiteport} -eq 0 ]; then
-	echo -n "Creating www dir \n"
+	echo -e "Creating www dir \n"
 	sudo mkdir -p $deploy_dir
 	sudo mkdir -p $log_dir
-	echo -n "Setting permissions \n"
+	echo -e "Setting permissions \n"
 	sudo chown -R $USER:$USER $deploy_dir
 
 	sudo chmod -R 755 /var/www
 
-	echo -n "Copying index.html file \n"
+	echo -e "Copying index.html file \n"
 	sudo cp vhc-includes/index.html $deploy_dir
-	echo -n "Create the New Virtual Host File \n"
+
+	echo -e "Create the New Virtual Host File \n"
 	echo "<VirtualHost *:80>
-		ServerAdmin webmaster@localhost
-		ServerName $domain_name
-		ServerAlias www.$domain_name
-		DocumentRoot /var/www/$domain_name/public_html
-		ErrorLog /var/www/$domain_name/logs/error.log
-		CustomLog /var/www/$domain_name/logs/access.log combined
-	</VirtualHost>" >> vhc-includes/virtual-host.conf
+	ServerAdmin webmaster@localhost
+	ServerName $domain_name
+	ServerAlias www.$domain_name
+	DocumentRoot /var/www/$domain_name/public_html
+	ErrorLog /var/www/$domain_name/logs/error.log
+	CustomLog /var/www/$domain_name/logs/access.log combined
+</VirtualHost>" >> vhc-includes/virtual-host.conf
 
 	sudo cp vhc-includes/virtual-host.conf /etc/apache2/sites-available/$domain_name.conf
 	
@@ -40,16 +43,16 @@ if [ ${#localsiteport} -eq 0 ]; then
 	sudo a2ensite $domain_name
 
 else
-	echo -n "Create the New Virtual Host File \n"
-	echo "Listen $localsiteport
-	<VirtualHost *:$localsiteport>
-		ServerAdmin webmaster@localhost
-		ServerName $domain_name
-		ServerAlias www.$domain_name
-		DocumentRoot /var/www/$domain_name/public_html
-		ErrorLog /var/www/$domain_name/logs/error.log
-		CustomLog /var/www/$domain_name/logs/access.log combined
-	</VirtualHost>" >> vhc-includes/virtual-host.conf
+	echo -e "Create the New Virtual Host File \n"
+echo "Listen $localsiteport
+<VirtualHost *:$localsiteport>
+	ServerAdmin webmaster@localhost
+	ServerName $domain_name
+	ServerAlias www.$domain_name
+	DocumentRoot /var/www/$domain_name/public_html
+	ErrorLog /var/www/$domain_name/logs/error.log
+	CustomLog /var/www/$domain_name/logs/access.log combined
+</VirtualHost>" >> vhc-includes/virtual-host.conf
 	sudo cp vhc-includes/virtual-host.conf /etc/apache2/sites-available/$domain_name:$localsiteport.conf
 	sudo a2ensite $domain_name:$localsiteport
 fi
