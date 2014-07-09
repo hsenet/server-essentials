0d250fbe0d13c62758e5c8c17cf0a73eb403137a
Jenkin Joyer Dsouza <jalann@gmail.com>
2014-07-09 17:41:25 +0800

    testing remove domain

--- remove-hosting.sh
+++ remove-hosting.sh
@@ -11,18 +11,30 @@ fi
 
 read -p "Enter a site port or press enter: " localsiteport
 
-read -e -p "Enter the user name associated with this domain:" -i "admin" user_name
+read -e -p "Enter the user name associated with this domain else press [enter]: " user_name
+
+deploy_dir=/var/www/$domain_name
 
 if [ ${#localsiteport} -eq 0 ]; then
+	echo -e "Disabling site ..."
 	sudo a2dissite $domain_name
+	echo -e "Removing vertual host entries ..."
 	sudo rm -rf /etc/apache2/sites-available/$domain_name.conf
 else
+	echo -e "Disabling site ..."
 	sudo a2dissite $domain_name:$localsiteport
+	echo -e "Removing vertual host entries ..."
 	sudo rm -rf /etc/apache2/sites-available/$domain_name:$localsiteport.conf
 fi
-echo -e "Reloading apache"
+
+
+echo -e "Reloading apache ..."
 service apache2 reload
 
 if [ ${#user_name} -ne 0 ]; then
-	userdel -r $user_name
+	echo -e "Removing user and all directories ..."
+	sudo userdel -r $user_name
+else 
+	echo -e "Removing www directory for this domain ..."
+	sudo rm -rf deploy_dir
 fi
